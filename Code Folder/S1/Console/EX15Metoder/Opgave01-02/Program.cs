5a5e9268abfcd24a07066fbfc6a0ffe6f911b842
Redd <150016078+Its-Redd@users.noreply.github.com>
2023-11-30 11:21:44 +0100

    Add files via upload

---
+++ Code Folder/S1/Console/EX15Metoder/Opgave01-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave01_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SayHello("Daniel");
+            SayGoodbye("Karsten");
+
+            Console.ReadKey();
+        }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello, {name}! Welcome!");
+        }
+
+        static void SayGoodbye(string name)
+        {
+            Console.WriteLine($"Goodbye, {name}! Take care!");
+        }
+    }
+}
---
+++ Code Folder/S1/Console/EX15Metoder/Opgave03-04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave03_04
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int maxNumber = FindMax(10, 15);
+            Console.WriteLine(maxNumber);
+
+            string[] names = { "Alice", "Bob", "Charlie" };
+            PrintArray(names);
+
+            Console.ReadKey();
+        }
+
+        static int FindMax(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+
+        static void PrintArray(string[] arr)
+        {
+            foreach (string element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
