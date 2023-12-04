3010d6ea4554077840e4ed9b40a0e2cf2c54bcbc
Redd <150016078+Its-Redd@users.noreply.github.com>
2023-12-04 11:58:31 +0100

    Stream reader assignment 1st commit
    Halfway done with this assigment

---
+++ Code Folder/S1/Console/EX19StreamReader/Opgave01-03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Opgave01_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader("C:\\Code Folder\\S1\\Console\\EX19StreamReader\\Opgave01-03\\Values.txt");
+
+            int sum = 0; // sum of values read
+            int count = 0; // number of values read
+
+            while (!sr.EndOfStream) // while not at the end of the file read a line
+            {
+                string line = sr.ReadLine();
+                int value = int.Parse(line);
+                sum += value;
+                count++;
+            }
+            sr.Close(); // close the file
+
+            //Print the sum and average
+            double average = (double)sum / count; // average of values read in a more precise value
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine($"Gennemsnit: {average}");
+
+            //-------------------------------------------------------------------------------------
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
