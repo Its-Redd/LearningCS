18f7e72258a6bae02fd2e5aba0805bc856988c12
Redd <150016078+Its-Redd@users.noreply.github.com>
2023-12-06 14:52:38 +0100

    Add files via upload

---
+++ Code Folder/S1/WPF/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using BIZ;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        ClassBIZ BIZ; // Create a new instance of ClassBIZ
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BIZ = new ClassBIZ(); // Initialize the instance of ClassBIZ
+        }
+
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender; // Create a new button object
+            Menu(int.Parse(button.Tag.ToString())); // Call the menu method with the tag of the button
+        }
+
+        private void Menu(int inKey)
+        {
+            ClearListBox(ListBoxRes); // Clear the listbox
+            switch (inKey)
+            {
+                case 1:
+                    BIZ.Number4711(ListBoxRes); // Call the method Number4711 from ClassBIZ
+                    break;
+                case 2:
+                    BIZ.RandomNumbers(ListBoxRes); // Call the method RandomNumbers from ClassBIZ
+                    break;
+                case 3:
+                    BIZ.RandomNumbersSorted(ListBoxRes); // Call the method RandomNumbersSorted from ClassBIZ
+                    break;
+                case 4:
+                    BIZ.SortedAndUnsorted(ListBoxRes); // Call the method SortedAndUnsorted from ClassBIZ
+                    break;
+                case 5:
+                    BIZ.RandomNumbersAVG(ListBoxRes); // Call the method RandomNumberAVG
+                    break;
+                case 6:
+                    BIZ.SortedRandomNumbersAVG(ListBoxRes);
+                    break;
+                case 7:
+                    ListBoxRes.ItemsSource = BIZ.SortedRandomNumbersAVGlist();
+                    break;
+                case 8:
+                    BIZ.EvenOrOdd(ListBoxRes);
+                    break;
+                case 9:
+                    MessageBox.Show("Opgave 10 er under opbygning", "Idc, skriv fejl, idk taber");
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        private void ClearListBox(ListBox listBox)
+        {
+            if (listBox != null)
+            {
+                listBox.ItemsSource = null;
+            }
+            listBox.Items.Clear();
+        }
+    }
+}
